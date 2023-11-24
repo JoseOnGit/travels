e86e433b1f0fe887f1b2d97a9bd338991fd0d9be
JoseOnGit <dodo.babjak@gmail.com>
2023-11-24 03:00:35 +0100

    bottom navigation update

--- src/components/BottomNav/BottomNavItem.tsx
+++ src/components/BottomNav/BottomNavItem.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from "react";
-import { Link } from "react-router-dom";
+import React, { FC, useState } from "react";
+import cx from "classnames";
+import { useNavigate } from "react-router-dom";
 import { NavItemType } from "../../types/types";
 
 import styles from "../../styles/navigation.module.scss";
@@ -9,11 +10,34 @@ type Props = {
 };
 
 const BottomNavItem: FC<Props> = ({ item }) => {
+  const navigate = useNavigate();
+
+  const [elementClasses, setElementClasses] = useState<string>();
+
+  const redirectToContitnent = () => {
+    localStorage.setItem("menuContinent", item.label);
+
+    setElementClasses(cx(styles.clicked, styles[item.label]));
+
+    window.setTimeout(() => {
+      setElementClasses("");
+      navigate(item.link);
+    }, 400);
+  };
+
   return (
-    <div className={styles.bottomNavIconWrapper}>
-      <Link to={item.link}>
-        <img key={item.id} alt={item.label} src={item.icon} />
-      </Link>
+    <div
+      className={styles.bottomNavIconWrapper}
+      onClick={() => redirectToContitnent()}
+    >
+      {/* <Link onClick={() => redirectToContitnent()} to={item.link}> */}
+      <img
+        className={elementClasses}
+        key={item.id}
+        alt={item.label}
+        src={item.icon}
+      />
+      {/* </Link> */}
     </div>
   );
 };
