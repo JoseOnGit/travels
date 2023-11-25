a63c4fb1f38905797f196168fbacea22dcdf53e3
JoseOnGit <dodo.babjak@gmail.com>
2023-11-25 13:29:22 +0100

    experimenting with globe animation 2

--- src/components/HeaderContinent.tsx
+++ src/components/HeaderContinent.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { ContinentType } from "../types/types";
+import cx from "classnames";
 
 import styles from "../styles/header.module.scss";
+import { useParams } from "react-router-dom";
 
 type Props = {
   continentData: ContinentType;
 };
 
 const HeaderContinent: FC<Props> = ({ continentData }) => {
+  const { continent } = useParams();
+
+  const [headerClasses, setHeaderClasses] = useState(styles.headerContinent);
+
+  useEffect(() => {
+    setHeaderClasses(cx(styles.headerContinent, styles.animate));
+    window.setTimeout(() => {
+      setHeaderClasses(styles.headerContinent);
+    }, 900);
+  }, [continent]);
+
   return (
-    <div className={styles.headerContinent}>
+    <div className={headerClasses}>
       <div className={styles.globe}>
         <img alt={continentData.label} src={continentData.globe} />
       </div>
