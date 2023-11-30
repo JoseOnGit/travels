4368a593898d228701383f7d0e712994a1da8e7d
JoseOnGit <dodo.babjak@gmail.com>
2023-11-30 18:06:36 +0100

    Botom navigation visual updates

--- src/components/BottomNav/BottomNavItem.tsx
+++ src/components/BottomNav/BottomNavItem.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import { NavItemType } from "../../types/types";
-
+import cx from "classnames";
 import styles from "../../styles/navigation.module.scss";
 
 type Props = {
   item: NavItemType;
+  selected: boolean;
 };
 
-const BottomNavItem: FC<Props> = ({ item }) => {
+const BottomNavItem: FC<Props> = ({ item, selected }) => {
+  const bottomNavIconClasses = selected
+    ? cx(styles.bottomNavIconWrapper, styles.selected)
+    : styles.bottomNavIconWrapper;
+
   return (
-    <div className={styles.bottomNavIconWrapper}>
+    <div className={bottomNavIconClasses}>
       <Link to={item.link}>
         <img key={item.id} alt={item.label} src={item.icon} />
       </Link>
--- src/components/BottomNav/BottomNavWrapper.tsx
+++ src/components/BottomNav/BottomNavWrapper.tsx
@@ -2,26 +2,32 @@ import React, { FC } from "react";
 import cx from "classnames";
 import { NavItemType } from "../../types/types";
 import { BottomNavItem } from "./BottomNavItem";
-import styles from "../../styles/navigation.module.scss";
-import "../../styles/main.module.scss";
 import { NavContext } from "../../contexts/NavProvider";
+import styles from "../../styles/navigation.module.scss";
 
 type Props = {
   navItems: NavItemType[];
+  selectedItem: string | undefined;
 };
 
-const BottomNavWrapper: FC<Props> = ({ navItems }) => {
+const BottomNavWrapper: FC<Props> = ({ navItems, selectedItem }) => {
   const { isOpen } = React.useContext(NavContext);
 
-  const classes = isOpen
+  const bottomNavWrapperClasses = isOpen
     ? cx(styles.bottomNavWrapper, styles.hidden)
     : styles.bottomNavWrapper;
 
   return (
-    <div className={classes}>
-      {navItems.map((navItem) => (
-        <BottomNavItem item={navItem} key={navItem.id} />
-      ))}
+    <div className={bottomNavWrapperClasses}>
+      {navItems.map((navItem) => {
+        return (
+          <BottomNavItem
+            item={navItem}
+            key={navItem.id}
+            selected={navItem.name === selectedItem}
+          />
+        );
+      })}
     </div>
   );
 };
