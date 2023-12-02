3476c186fccea2120f3f50b6d3932326aa04b237
JoseOnGit <dodo.babjak@gmail.com>
2023-12-02 05:32:57 +0100

    Globe animation update/refactor

--- src/components/TripList/TripList.tsx
+++ src/components/TripList/TripList.tsx
@@ -1,50 +1,16 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 import { TripType } from "../../types/types";
-import cx from "classnames";
 import { TripListItem } from "./TripListItem";
 import styles from "../../styles/tripMenu.module.scss";
-import { NavContext } from "../../contexts/NavProvider";
-import { toggleClasses } from "../../utils/utils";
 
 type Props = {
   continentTrips: TripType[];
 };
 
 const TripList: FC<Props> = ({ continentTrips }) => {
-  const { pageLoading, pageLeaving } = React.useContext(NavContext);
+  // const { pageLoading, pageLeaving } = React.useContext(NavContext);
 
-  const [tripListClasses, setTripListClasses] = useState(
-    styles.tripMenuWrapper
-  );
-
-  // useEffect(() => {
-  //   toggleClasses(
-  //     styles.tripMenuWrapper,
-  //     styles.animate,
-  //     pageLoading,
-  //     setTripListClasses
-  //   );
-  //   // if (pageLoading) {
-  //   //   setTripListClasses(cx(styles.tripMenuWrapper, styles.animate));
-  //   //   window.setTimeout(() => {
-  //   //     setTripListClasses(styles.tripMenuWrapper);
-  //   //   }, 700);
-  //   // }
-  // }, [pageLoading]);
-
-  // useEffect(() => {
-  //   toggleClasses(
-  //     styles.tripMenuWrapper,
-  //     cx(styles.animate, styles.out),
-  //     pageLeaving,
-  //     setTripListClasses
-  //   );
-  //   // if (pageLeaving) {
-  //   //   setTripListClasses(
-  //   //     cx(styles.tripMenuWrapper, styles.animate, styles.out)
-  //   //   );
-  //   // }
-  // }, [pageLeaving]);
+  const [tripListClasses] = useState(styles.tripMenuWrapper);
 
   return (
     <div className={tripListClasses}>
